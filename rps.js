fca0e4fc134aa4203d550675790d466ef8ffccf8
Dani <daninovet@proton.me>
2023-02-20 12:44:03 -0600

    modify logic to display messages on page

--- rps.js
+++ rps.js
@@ -1,92 +1,65 @@
 //open console and initialize with game();//
 function getComputerChoice(items) {
- return items[Math.floor(Math.random() * items.length)];
-}
+    return items[Math.floor(Math.random() * items.length)];
+   }
+   
+   const items = ["rock","paper","scissors"]
+   
+   const R = document.querySelector("#R");
+   R.addEventListener("click", function(){
+       return playerSelection = "rock"
+   });
+   R.addEventListener("click", playRound);
+   
+   const P = document.querySelector("#P"); 
+   P.addEventListener("click", function(){
+       return playerSelection = "paper"
+   });
+   P.addEventListener("click",playRound);
+   
+   const S = document.querySelector("#S");
+   S.addEventListener("click", function(){
+       return playerSelection = "scissors"
+   }); 
+   S.addEventListener("click", playRound)
 
-const items = ["rock","paper","scissors"]
-const round = ["Round 1", "Round 2", "Round 3", "Round 4", "Round 5"]
+    let playerScore = 0
+    let computerScore = 0
+   function playRound() {
+        const score = document.getElementById("score")
+        const computerSelection = getComputerChoice(items)
 
-let playerScore = 0
-let computerScore = 0
-
-function userInput(choice){
-    return choice
-}
-
-const R = document.querySelector("#R");
-R.addEventListener("click", function(){
-    return playerSelection = "rock"
-});
-R.addEventListener("click", playRound);
-
-
-const P = document.querySelector("#P"); 
-P.addEventListener("click", function(){
-    return playerSelection = "paper"
-});
-P.addEventListener("click",playRound);
-
-const S = document.querySelector("#S");
-S.addEventListener("click", function(){
-    return playerSelection = "scissors"
-}); 
-S.addEventListener("click", playRound)
-
-function playRound() {
-     
-     const computerSelection = getComputerChoice(items)
-
-     if (playerSelection == "rock" && computerSelection == "paper") {
-        console.log("You: " + playerSelection);
-        console.log("Computer: " + computerSelection);
-        return computerScore++
-    }
-    
-    else if (playerSelection == "rock" && computerSelection == "scissors") {
-        console.log("You: " + playerSelection);
-        console.log("Computer: " + computerSelection);
-        return playerScore ++
-    }
-    
-    else if (playerSelection == "rock" && computerSelection == "rock") {
-        console.log("You: " + playerSelection);
-        console.log("Computer: " + computerSelection);
+        if (playerScore === 5){
+            score.textContent = "YOU WIN";
+       
+        }else if (computerScore === 5){
+            score.textContent = "COMPUTER WINS";
+   
+        }else if (playerSelection == "rock" && computerSelection == "paper") {
+            score.textContent = "You: " + playerSelection + " Computer: " + computerSelection + " Player Score: " + playerScore + " Computer Score: " + ++computerScore;
+            
+        }else if (playerSelection == "rock" && computerSelection == "scissors") {
+        score.textContent = "You: " + playerSelection + " Computer: " + computerSelection + " Player Score: " + ++playerScore + " Computer Score: " + computerScore;
+       
+        }else if (playerSelection == "rock" && computerSelection == "rock") {
+                score.textContent = "You: " + playerSelection + " Computer: " + computerSelection + " Player Score: " + playerScore + " Computer Score: " + computerScore;
         
-    }
-    
-    else if (playerSelection == "paper" && computerSelection == "rock") {
-        console.log("You: " + playerSelection);
-        console.log("Computer: " + computerSelection);
-        return playerScore++
-    }
-    
-    else if (playerSelection == "paper" && computerSelection == "scissors") {
-        console.log("You: " + playerSelection);
-        console.log("Computer: " + computerSelection);
-        return computerScore++
-    }
-    
-    else if (playerSelection == "paper" && computerSelection == "paper") {
-        console.log("You: " + playerSelection);
-        console.log("Computer: " + computerSelection);
-    }
-    
-    else if (playerSelection == "scissors" && computerSelection == "paper") {
-        console.log("You: " + playerSelection);
-        console.log("Computer: " + computerSelection);
-        return playerScore++   
-    }
-    
-    else if (playerSelection == "scissors" && computerSelection == "rock") {
-        console.log("You: " + playerSelection);
-        console.log("Computer: " + computerSelection);
-        return computerScore++
-    }
-    
-    else if (playerSelection == "scissors" && computerSelection == "scissors") {
-        console.log("You: " + playerSelection);
-        console.log("Computer: " + computerSelection);
-    }
-    
-}
-
+        }else if (playerSelection == "paper" && computerSelection == "rock") {
+                score.textContent = "You: " + playerSelection + " Computer: " + computerSelection + " Player Score: " + ++playerScore + " Computer Score: " + computerScore;
+        
+        }else if (playerSelection == "paper" && computerSelection == "scissors") {
+                score.textContent = "You: " + playerSelection + " Computer: " + computerSelection + " Player Score: " + playerScore + " Computer Score: " + ++computerScore;
+        
+        }else if (playerSelection == "paper" && computerSelection == "paper") {
+            score.textContent = "You: " + playerSelection + " Computer: " + computerSelection + " Player Score: " + playerScore + " Computer Score: " + computerScore;
+        
+        }else if (playerSelection == "scissors" && computerSelection == "paper") {
+            score.textContent = "You: " + playerSelection + " Computer: " + computerSelection + " Player Score: " + ++playerScore + " Computer Score: " + computerScore;    
+        }else if (playerSelection == "scissors" && computerSelection == "rock") {
+            score.textContent = "You: " + playerSelection + " Computer: " + computerSelection + " Player Score: " + playerScore + " Computer Score: " + ++computerScore;
+        
+        }else if (playerSelection == "scissors" && computerSelection == "scissors") {
+            score.textContent = "You: " + playerSelection + " Computer: " + computerSelection + " Player Score: " + playerScore + " Computer Score: " + computerScore;
+        }
+       
+       }
